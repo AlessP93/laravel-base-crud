9209c785dc93a90586da269ffa61436454378a67
AlessP93 <alespiscino@libero.it>
2022-07-15 16:07:24 +0200

    inizio

---
+++ database/migrations/2022_07_14_141055_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 80);
+            $table->string('type', 50);
+            $table->string('series', 50);
+            $table->text('description');
+            $table->decimal('price', 5, 2);
+            $table->date('sale_date');
+            $table->string('thumb')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
